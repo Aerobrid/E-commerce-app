ff0fb4c8ad5e92d6f59de0c45dbab11429771a92
Raj Patel <raj721patel@gmail.com>
2025-03-09 17:17:27 -0500

    Experimenting #1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header.jsx'
+import Header from './components/header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Header/> 
+    <div>
+      <Header/>
+      <main className='main-stuff'>
+        <h1>Welcome to the E-commerce App</h1>
+        <p>Current count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
